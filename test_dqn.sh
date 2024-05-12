1209c75aaf20b0bb94ac0c7f609f4211c3b720f5
Tony Cui <tacui@mit.edu>
2024-05-12 04:44:28 -0400

    test scripts

---
+++ test_dqn.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source /etc/profile
+
+module load anaconda/2023a
+
+source activate b
+
+python sample_dqn.py
+
+# # Fail on any error.
+# set -e
+# # Display commands being run.
+# set -x
+
+# # Set up a new virtual environment.
+# python3 -m venv bsuite_testing
+# source bsuite_testing/bin/activate
+
+# # Install all dependencies.
+# pip install --upgrade pip setuptools
+# pip install .
+# pip install .[baselines_jax]
+# pip install .[baselines]
+
+# # Install test dependencies.
+# pip install .[testing]
+
+# N_CPU=$(grep -c ^processor /proc/cpuinfo)
+
+# # Run static type-checking.
+# pytype -j "${N_CPU}" bsuite
+
+# # Run all tests.
+# pytest -n "${N_CPU}" bsuite
+
+# # Clean-up.
+# deactivate
+# rm -rf bsuite_testing/
+
