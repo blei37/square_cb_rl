1831c2c111c7058119db4e6a87f70d682fc45daf
Si Liang Lei <siliang.lei@gmail.com>
2024-05-12 23:13:19 -0400

    added graphs

--- sample.py
+++ sample.py
@@ -19,20 +19,57 @@ import warnings
 import numpy as np
 import pandas as pd
 import plotnine as gg
+import matplotlib.pyplot as plt
 
 pd.options.mode.chained_assignment = None
 gg.theme_set(gg.theme_bw(base_size=16, base_family='serif'))
 gg.theme_update(figure_size=(12, 8), panel_spacing_x=0.5, panel_spacing_y=0.5)
 warnings.filterwarnings('ignore')
 
-print("STARTING DATA ANALYSIS\n")
+envs = [
+      "bsuite_id_-_bandit_noise-0.csv",
+      "bsuite_id_-_bandit-0.csv",
+      "bsuite_id_-_cartpole_noise-11.csv",
+      "bsuite_id_-_cartpole-0.csv",
+      "bsuite_id_-_deep_sea_stochastic-2.csv",
+      "bsuite_id_-_umbrella_length-19.csv",
+]
+
+env_cat = [
+      'Bandit-Noise',
+      'Bandit',
+      'Cartpole-Noise',
+      'Cartpole',
+      'Deep-Sea-Stochastic',
+      'Umbrella-Length'
+]
+
+agent_paths = ["dqn", "dqn_squarecb", "dqn_ucb"]
+
+for i in range(len(envs)):
+      env_name = envs[i]
+      print('\nENV_NAME:', env_name)
+      plt.figure(figsize=(10, 6))
+      for agent_path in agent_paths:
+            print('AGENT:', agent_path)
+            df = pd.read_csv("./logs" + "/" + agent_path + "/" + env_name)
+            plt.plot(df['steps'], df['episode_return'], marker='o', linestyle='-', label=agent_path)
+      plt.title(env_cat[i] + ' Steps vs Episode Return')
+      plt.xlabel('Steps')
+      plt.ylabel('Episode Return')
+      plt.legend()
+      plt.grid(True)
+      plt.show()
+
+
+# print("STARTING DATA ANALYSIS\n")
 # for bsuite_id in sweep.SWEEP:
 #       env = bsuite.load_from_id(bsuite_id)
 #       print('bsuite_id={}, settings={}, num_episodes={}'
 #         .format(bsuite_id, sweep.SETTINGS[bsuite_id], env.bsuite_num_episodes))
 
 # ADD AGENTS NAMES HERE
-agent_names = ['dqn', 'dqn_squarecb', 'dqn_ucb']
+# agent_names = ['dqn', 'dqn_squarecb', 'dqn_ucb']
 # def getEnvAgent(bsuite_id, ag):
 #       SAVE_PATH = './logs/' + ag
 #       ENV = bsuite.load_and_record(bsuite_id, save_path=SAVE_PATH, overwrite=True)
@@ -59,17 +96,17 @@ agent_names = ['dqn', 'dqn_squarecb', 'dqn_ucb']
 #             cur_env, cur_ag = getEnvAgent(bsuite_id, ag_name)
 #             experiment.run(cur_ag, cur_env, num_episodes=cur_env.bsuite_num_episodes)
 
-experiments = {}
-for ag_name in agent_names:
-      ag_path = './logs/' + ag_name
-      experiments[ag_name] = ag_path
+# experiments = {}
+# for ag_name in agent_names:
+#       ag_path = './logs/' + ag_name
+#       experiments[ag_name] = ag_path
 
-DF, SWEEP_VARS = csv_load.load_bsuite(experiments)
-BSUITE_SCORE = summary_analysis.bsuite_score(DF, SWEEP_VARS)
-BSUITE_SUMMARY = summary_analysis.ave_score_by_tag(BSUITE_SCORE, SWEEP_VARS)
-print("BSUITE_SCORE:", BSUITE_SCORE)
-print("BSUITE_SUMMARY:", BSUITE_SUMMARY)
-bandit_noise_df = DF[DF.bsuite_env == 'bandit_noise'].copy()
-summary_analysis.plot_single_experiment(BSUITE_SCORE, 'bandit_noise', SWEEP_VARS)
-bandit_noise_analysis.plot_average(bandit_noise_df, SWEEP_VARS) 
+# DF, SWEEP_VARS = csv_load.load_bsuite(experiments)
+# BSUITE_SCORE = summary_analysis.bsuite_score(DF, SWEEP_VARS)
+# BSUITE_SUMMARY = summary_analysis.ave_score_by_tag(BSUITE_SCORE, SWEEP_VARS)
+# print("BSUITE_SCORE:", BSUITE_SCORE)
+# print("BSUITE_SUMMARY:", BSUITE_SUMMARY)
+# bandit_noise_df = DF[DF.bsuite_env == 'bandit_noise'].copy()
+# summary_analysis.plot_single_experiment(BSUITE_SCORE, 'bandit_noise', SWEEP_VARS)
+# bandit_noise_analysis.plot_average(bandit_noise_df, SWEEP_VARS) 
 # __radar_fig__ = summary_analysis.bsuite_radar_plot(BSUITE_SUMMARY, SWEEP_VARS)
